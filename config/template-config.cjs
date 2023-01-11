670df3556f7d8ddb437549b94462198b4e98bf7f
Yuncryy <109648123+Yuncryy@users.noreply.github.com>
2023-01-11 22:24:07 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '致最最最最最可爱的飞宝',
     desc: `
       **{{date.DATA}}**
       
@@ -19,20 +19,31 @@ const TEMPLATE_CONFIG = [
       风向: {{wind_direction.DATA}}
       
       风级: {{wind_scale.DATA}}
+
+      湿度:{{shidu.DATA}}
+     
+      {{notice.DATA}}
+
+
+      ---
       
+      {{earthy_love_words.DATA}}
+
+      ---
+
+      瞧瞧双鱼宝宝今天的运势呀！
+
       {{comprehensive_horoscope.DATA}}
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
+      今天是我们在一起的第{{love_day.DATA}}天！
+
+      不想中断啊~不想中断啊~不想中断啊~啊啊啊啊啊！
+
       ---
       
-      {{moment_copyrighting.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
   },
 ]
